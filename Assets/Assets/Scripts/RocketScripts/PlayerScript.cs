928dd3aee7b87a9c74adfc29e08f4f569a03de40
Jonatan Karlsson <Jk98@Live.se>
2019-10-27 14:18:15 +0100

    #6

--- Assets/Assets/Scripts/RocketScripts/PlayerScript.cs
+++ Assets/Assets/Scripts/RocketScripts/PlayerScript.cs
@@ -40,7 +40,7 @@ namespace SpaceWarsOnline
             }
              else if (!photonView.IsMine)
              {
-                GetComponent<PhotonView>().RPC("ChangeName", RpcTarget.AllBuffered);
+                GetComponent<PhotonView>().RPC("ChangeName", RpcTarget.MasterClient);
             }
            
         }

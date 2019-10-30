b92060c316520816c8216ef28008cd52498eb99e
Jonatan Karlsson <Jk98@Live.se>
2019-10-30 12:06:38 +0100

    #19

--- Assets/Assets/Scripts/NetworkScripts/GameManagerScript.cs
+++ Assets/Assets/Scripts/NetworkScripts/GameManagerScript.cs
@@ -54,7 +54,7 @@ namespace SpaceWarsOnline
             PhotonNetwork.SerializationRate = 10;
             Debug.Log("Start Called: Game Manager");
             //numberOfPlayers += 1;
-            PlayerScript.RefreshInstance(ref localPlayer, playerPrefab, spawnLocation[numberOfPlayers]);
+            PlayerScript.RefreshInstance(ref localPlayer, playerPrefab, spawnLocation[PhotonNetwork.PlayerList.Length/*numberOfPlayers*/]);
 
            
         }    

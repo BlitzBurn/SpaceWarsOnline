c317c8829ae18e8dff7c56bb15b29b3b33dc3a4c
Jonatan Karlsson <Jk98@Live.se>
2019-12-15 19:43:37 +0100

    e

--- Assets/Assets/Scripts/RocketScripts/FireMissile.cs
+++ Assets/Assets/Scripts/RocketScripts/FireMissile.cs
@@ -31,7 +31,7 @@ public class FireMissile :MonoBehaviourPun
         time += Time.deltaTime;
         if (GameManagerScript.gameIsInProgress && _playerScript.playerIsAlive)
         {
-            if (Input.GetKeyDown(KeyCode.Mouse1) && time >= MissileCooldown)
+            if (Input.GetKeyDown(KeyCode.E) && time >= MissileCooldown)
             {
                 FireMissileMethod();
 
